e651e4181a378e52865283b511009ebe3dcf5e75
kyletm <newportkyle@gmail.com>
2017-09-11 14:36:13 -0400

    Utils refactoring, moving R dependencies

--- model/utils/core.py
+++ model/utils/core.py
@@ -1,4 +1,22 @@
+import os
+import subprocess
 
+def sort_by_input_column(input_path, input_file, sort_column, output_path, output_file):
+   """Sort a file by a specified column.
+
+   Inputs:
+       input_path (str): Path to input file.
+       input_file (str): Input file name.
+       sort_column (str): Numeric column to sort.
+       output_path (str): Path to output file.
+       output_file: Output file name.
+   """
+   base_module_path = os.path.dirname(os.path.realpath(__file__))
+   script_path = base_module_path + '/' + 'sort_by_input_column.r'
+   subprocess.call(["C:/R/R-3.3.1/bin/Rscript.exe", script_path,
+                     input_path, input_file, sort_column,
+                     output_path, output_file])
+                     
 def cdf(weights):
     """Create CDF of weighted list.
     
