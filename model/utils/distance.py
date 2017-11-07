71233232582cd09b9fb27af53a4370f2bf231f10
kyletm <newportkyle@gmail.com>
2017-11-06 21:07:35 -0500

    docs: Distance documentation
    Added documentation to explain distance function and refactored common
    radius variable for clarity.

--- model/utils/distance.py
+++ model/utils/distance.py
@@ -3,7 +3,19 @@ Module for distance related functionality.
 '''
 import math
 
+# equitorial radius of the earth, in miles
+RADIUS = 3963.167
+
 def between_points(lat1, lon1, lat2, lon2):
+    """Computes great circle distance between two lat-lon pairs.
+
+    Inputs:
+        lat1, lon1 (float): First coordinate pair.
+        lat2, lon2 (float): Second coordinate pair.
+    
+    Returns:
+        dist (float): Great circle distance between two points.
+    """
     degrees_to_radians = math.pi/180.0
     phi1 = (90.0 - float(lat1))*degrees_to_radians
     phi2 = (90.0 - float(lat2))*degrees_to_radians
@@ -12,4 +24,4 @@ def between_points(lat1, lon1, lat2, lon2):
     cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + 
            math.cos(phi1)*math.cos(phi2))
     arc = math.acos(cos)
-    return arc * 3963.167
+    return arc * RADIUS
