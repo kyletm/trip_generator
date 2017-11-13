c8c6b23d01dbe18e2dc6d0f542409fbe9f29aa94
kyletm <newportkyle@gmail.com>
2017-11-13 13:07:26 -0500

    fix: Changed pixel calculation
    Corrected pixel coordinate transformation. Attempted to correct centroid,
    but not correct yet. See #TODO example for more info.
    Github Issue #18

--- model/module5/pixel.py
+++ model/module5/pixel.py
@@ -7,38 +7,38 @@ Created on Fri Apr 14 13:45:02 2017
 
 import math
 
-def find_pixel_centroid(x,y):
-    """ 
-    Summary:
-    Returns the lat-long centroid of a given x-y pixel coordinates
+
+# TODO - This function does not properly compute the centroid and only
+# works well close to the starting point - avoid use for now
+# see below for example of failure
+# find_pixel_coords(find_pixel_centroid(1800, -308)[0],
+#                   find_pixel_centroid(1800, -308)[1])
+def find_pixel_centroid(x, y):
+    """Returns the lat-lon corner of a given x-y pixel coordinates
     
-    Input Arguments:    
-    x,y: An x-y pixel pair of a given coordinate
+    Inputs:    
+        x,y (int): An x-y pixel pair of a given coordinate
     
-    Output:
-    The estimated lat-long centroid of a given x-y pixel
+    Returns:
+        lat, lon (float): lat, lon corner of a given x-y pixel coordinate
     """
     # See formulas from find_pixel_coords for more info
-    x1 = 75.6-x/108.907 
-    x2 = 75.6-(x+1)/108.907
-    y1 = y/138.2 + 38
-    y2 = (y+1)/138.2 + 38 
-    return -(x1+x2)/2,(y1+y2)/2
+    x, y = int(x), int(y)
+    lat = y/134.348 + 37.0
+    lon = x/(134.348*math.cos(math.pi/180 * lat)) - 97.5
+    return lat, lon
     
 
 def find_pixel_coords(lat, lon):
-    """ 
-    Summary:
-    Returns the pixel coordinates of a given lat-lon on a uniform pixel
-    grid centered in New Jersey
+    """Returns pixel coordinates on a uniform grid centered on U.S.
     
-    Input Arguments:    
-    lat, lon: A lat-long pair to convert to a pixel
+    Inputs:    
+        lat, lon (float): A lat-long pair to convert to a pixel
     
-    Output:
-    The X and Y Coordinates of the pixel to which this lat-long belongs
+    Returns:
+        xCoord, yCoord (int): x, y pixel coordinates
     """
     lat, lon = float(lat), float(lon)
-    xCoord = math.floor(108.907*(lon+75.6))
-    yCoord = math.floor(138.2*(lat-38.9))
+    xCoord = math.floor(138.348*(lon+97.5)*math.cos(lat*math.pi/180))
+    yCoord = math.floor(138.348*(lat-37.0))
     return xCoord, yCoord
